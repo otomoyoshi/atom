9f84c835fc95a8b7728ea557d95620f4e3374ed7
otomoyoshi <tomoyoshi.o.ib@gmail.com>
2017-09-12 22:10:04 +0800

    実験終了

--- theme/template/home.php
+++ theme/template/home.php
@@ -129,7 +129,16 @@ var_dump($results);
 
   <!-- ログインをしてるときとそうでないときで読み込むヘッダを変える -->
   <?php
-
+    // $ini = parse_ini_file("config.ini");
+    // $is_login = $ini['is_login'];
+    // $is_login = 0; //ログインしてるときを１とする（仮）
+    // if ($_SESSION['login_user']) { //ログインしてるとき
+    //   // echo "login success";
+    //   require('login_header.php');
+    // } else {// ログインしてないとき
+    //   // echo "login fail";
+    //   require('header.php');
+    // }
   ?>
   <div id="headerwrap">
     <div class="container">
