b8a5a098824b61abec8e4c4946004739055f7dcc
otomoyoshi <tomoyoshi.o.ib@gmail.com>
2017-09-14 11:27:51 +0800

    カテゴリー修正

--- theme/template/home.php
+++ theme/template/home.php
@@ -411,6 +411,7 @@ foreach ($results_l3 as $result_l3) {
               ?>
                     <div class="row dev_border">
                   <?php } ?>
+                    <!-- <div class="col-lg-2 text-center tabs" id="tab3_<?php echo $i ?>"> -->
                     <div class="col-lg-2 text-center tabs" id="tab3_<?php echo $i ?>">
                       <?php echo $results_l3[$j]['word']; ?>
                     </div>
