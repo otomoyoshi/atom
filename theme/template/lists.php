e3d7bc0b7d4fefc50558d46f24e5b3f6695acea8
Taiki__ck <tensainanoda@gmail.com>
2017-09-26 21:32:14 +0800

    ヘッダーつけました！

--- theme/template/lists.php
+++ theme/template/lists.php
@@ -24,17 +24,17 @@
   <!-- ログインをしてるときとそうでないときで読み込むヘッダを変える -->
 
   <?php
-    // $ini = parse_ini_file("config.ini");
-    // $is_login = $ini['is_login'];
-    // $is_login = 0; //ログインしてるときを１とする（仮）
-    // if (isset($_SESSION['login_user'])){ //ログインしてるとき
-    //   // echo "login success";
-    //   require('login_header.php');
+    $ini = parse_ini_file("config.ini");
+    $is_login = $ini['is_login'];
+    $is_login = 0; //ログインしてるときを１とする（仮）
+    if (isset($_SESSION['login_user'])){ //ログインしてるとき
+      // echo "login success";
+      require('login_header.php');
 
-    // } else {// ログインしてないとき
-    //   // echo "login fail";
-    //   require('header.php');
-    // }
+    } else {// ログインしてないとき
+      // echo "login fail";
+      require('header.php');
+    }
   ?>
 
 <!-- 画像変更popup -->
